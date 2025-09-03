ca0e62e7cb1661aac25482f63291ee3a8e2ba739
PhongLe7de <phongle260260@gmail.com>
2025-09-03 14:38:44 +0700

    create db schema and boards table

--- backend/src/main/java/com/otp/whiteboard/model/User.java
+++ backend/src/main/java/com/otp/whiteboard/model/User.java
@@ -33,7 +33,6 @@ public class User {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LibraryUseConstructor
     public User() {
     }
 
