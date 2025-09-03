fdec04d14c9ce604cf11dfcfd52ad2f3ed29e769
Casper <casper141@gmail.com>
2025-09-03 15:58:20 +0300

    Initial functions for broadcasting cursor position and drawing events to the backend.
    useWebSocket hook created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import Canvas from "@/components/Canvas.tsx";
 import { AppSidebar } from "@/components/AppSidebar.tsx";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import type {DrawingEvent} from "@/types.ts";
+import useWebSocket from "@/hooks/useWebSocket.tsx";
+
+
 
 function App() {
     const [userData, setUserData] = useState(undefined)
@@ -9,6 +13,22 @@ function App() {
     const [tool, setTool] = useState("pen");
     const [brushSize, setBrushSize] = useState(5);
     const [brushColor, setBrushColor] = useState("#000000");
+    const {
+        isConnected,
+        sendDrawingEvent,
+        sendCursorPosition,
+    } = useWebSocket();
+
+
+    const handleDrawingEvent = (event: DrawingEvent) => {
+        // TODO: send drawing event when backend is ready
+        sendDrawingEvent(event);
+    };
+
+    const handleCursorMovement = (username: string, x: number, y: number) => {
+        // TODO: create Cursor component send cursor position when backend is ready
+        sendCursorPosition(username, x, y);
+    };
 
     return (
         <SidebarProvider>
@@ -21,6 +41,8 @@ function App() {
                     tool={tool}
                     brushSize={brushSize}
                     brushColor={brushColor}
+                    onDrawingEvent={handleDrawingEvent}
+                    onCursorMove={handleCursorMovement}
                 />
             </main>
         </SidebarProvider>
---
+++ frontend/src/hooks/useWebSocket.tsx
@@ -0,0 +1,57 @@
+import type {DrawingEvent} from "@/types.ts";
+import {Client} from "@stomp/stompjs";
+import {useEffect, useRef, useState} from "react";
+
+// TODO: api destinations for broadcasting
+const useWebSocket = () => {
+    const clientRef = useRef<Client | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+
+    // https://stomp-js.github.io/guide/stompjs/using-stompjs-v5.html
+    // Initialize stomp client and add subs and publish routes
+    useEffect(() => {
+        const client = new Client({
+            brokerURL: import.meta.env.WS_API_URL,
+            // debug: function (str) {
+            //     console.log("STOMP Debug:", str);
+            // },
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+        });
+
+        client.onConnect = () => {
+            console.log("Connected to STOMP");
+            setIsConnected(true);
+
+        };
+
+        client.onDisconnect = () => {
+            console.log("Disconnected from STOMP");
+            setIsConnected(false);
+        };
+
+        client.onStompError = (frame) => {
+            console.error("STOMP Error:", frame);
+        };
+
+        client.activate();
+        clientRef.current = client;
+
+        // Cleanup on unmount
+        return () => {
+            client.deactivate();
+        };
+    }, []);
+
+    const sendDrawingEvent = (event: DrawingEvent) => {
+        console.log("Sending event: ", event);
+    };
+
+    const sendCursorPosition = (username: string, x: number, y: number) => {
+    };
+
+    return { isConnected, sendDrawingEvent, sendCursorPosition };
+}
+
+export default useWebSocket;
