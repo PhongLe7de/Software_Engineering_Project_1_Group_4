6a399db06d8b906d02dfc1768a9c2ab46e238867
PhongLe7de <phongle260260@gmail.com>
2025-09-19 17:33:52 +0700

    authentication and authorization

--- backend/src/main/java/com/otp/whiteboard/api/controller/UserController.java
+++ backend/src/main/java/com/otp/whiteboard/api/controller/UserController.java
@@ -6,10 +6,13 @@ import com.otp.whiteboard.dto.user.UserDto;
 
 import com.otp.whiteboard.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +21,7 @@ import static com.otp.whiteboard.api.Endpoint.USER_INTERNAL_API;
 
 @RestController
 @RequestMapping(USER_INTERNAL_API)
+@SecurityRequirement(name = "Bearer Authentication")
 public class UserController {
 
     private final UserService userService;
@@ -27,11 +31,11 @@ public class UserController {
     }
 
     @Operation(
-        summary = "Get all users",
-        description = """
-                This endpoint retrieves a list of all users in the system.
-                It returns a JSON array containing user details such as ID, display name, email, and profile picture URL.
-                """
+            summary = "Get all users",
+            description = """
+                    This endpoint retrieves a list of all users in the system.
+                    It returns a JSON array containing user details such as ID, display name, email, and profile picture URL.
+                    """
     )
     @GetMapping("/all")
     @ResponseBody
@@ -41,18 +45,18 @@ public class UserController {
     }
 
     @Operation(
-        summary = "Get user profile by display name",
-        description = """
-                This endpoint retrieves a user's profile information based on their display name.
-                It accepts a JSON payload containing the display name and returns the corresponding user's details.
-                If no user is found with the provided display name, an appropriate error message is returned.
-                """
+            summary = "Get user profile by display name",
+            description = """
+                    This endpoint retrieves a user's profile information based on their display name.
+                    It accepts a JSON payload containing the display name and returns the corresponding user's details.
+                    If no user is found with the provided display name, an appropriate error message is returned.
+                    """
     )
     @PostMapping("/profile")
     @ResponseBody
     public ResponseEntity<UserDto> getUserProfileByDisplayName(
             @RequestBody
-            @NotNull (message = "request must not be null")
+            @NotNull(message = "request must not be null")
             @Valid final UserByDisplayNameRequest request) {
         UserDto userDto = userService.getUserProfileByDisplayName(request);
         return ResponseEntity.ok(userDto);
@@ -60,17 +64,18 @@ public class UserController {
 
     /**
      * Endpoint to update user details.
-     * @param id The ID of the user to update.
+     *
+     * @param id      The ID of the user to update.
      * @param request The user update request containing new details.
      */
     @PutMapping("/update/{id}")
     @ResponseBody
     public ResponseEntity<UserDto> updateUser(
             @PathVariable
-            @NotNull (message = "Id must not be null")
+            @NotNull(message = "Id must not be null")
             @Positive(message = "Id must be positive") final Long id,
             @RequestBody @NotNull(message = "request must not be null") @Valid final UserUpdateRequest request) {
-    UserDto updatedUser = userService.updateUser(id, request);
+        UserDto updatedUser = userService.updateUser(id, request);
         return ResponseEntity.ok(updatedUser);
     }
 }
