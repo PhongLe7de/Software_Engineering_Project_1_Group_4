3362332b783f94ab78143414d01318283f69d0ee
Le Phong <phongle@Mac-mini-cua-Le.local>
2025-09-24 13:21:23 +0300

    board controller

--- backend/src/main/java/com/otp/whiteboard/api/controller/BoardController.java
+++ backend/src/main/java/com/otp/whiteboard/api/controller/BoardController.java
@@ -1,4 +1,124 @@
 package com.otp.whiteboard.api.controller;
 
+
+import com.otp.whiteboard.dto.board.BoardCreatingRequest;
+import com.otp.whiteboard.dto.board.BoardDto;
+import com.otp.whiteboard.dto.board.BoardUpdateRequest;
+import com.otp.whiteboard.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.otp.whiteboard.api.Endpoint.BOARD_INTERNAL_API;
+
+@RestController
+@RequestMapping(BOARD_INTERNAL_API)
+@SecurityRequirement(name = "Bearer Authentication")
 public class BoardController {
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @Operation(
+            summary = "Create a new board",
+            description = """
+                    This endpoint creates a new board based on the provided details.
+                    It accepts a JSON payload containing the board name and description, and returns the created board's details.
+                    """
+    )
+    @PostMapping("/create")
+    @ResponseBody
+    public ResponseEntity<BoardDto> createBoard(
+            @RequestBody
+                    @NotNull
+                    @Valid final
+            BoardCreatingRequest request
+    ) {
+        BoardDto boardDto = boardService.createBoard(request);
+        return ResponseEntity.ok(boardDto);
+    }
+
+    @Operation(
+            summary = "Add a user to a board"
+            ,description = """
+            This endpoint adds a user to a specified board based on the provided board ID and user ID.
+            It accepts the board ID and user ID as request parameters and returns the updated board details.
+            """
+    )
+    @PostMapping("/edit/addUser")
+    public ResponseEntity<BoardDto> addUserToBoard(
+            @RequestParam("boardId") Long boardId,
+            @RequestParam("userId") Long userId
+    ) {
+        BoardDto response = boardService.addUserToBoard(boardId, userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Remove a user from a board"
+            ,description = """
+            This endpoint removes a user from a specified board based on the provided board ID and user ID.
+            It accepts the board ID and user ID as request parameters and returns the updated board details.
+            """
+    )
+    @PostMapping("/edit/removeUser")
+    public ResponseEntity<BoardDto> removeUserFromBoard(
+            @RequestParam("boardId") Long boardId,
+            @RequestParam("userId") Long userId
+    ) {
+        BoardDto response = boardService.removeUserFromBoard(boardId, userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Update an existing board",
+            description = """
+                    This endpoint updates the details of an existing board identified by its ID.
+                    It accepts a JSON payload containing the updated board information and returns a success message upon completion.
+                    """
+    )
+    @PutMapping("/update/{boardId}")
+    public ResponseEntity<BoardDto> updateBoard(
+            @PathVariable("boardId") Long boardId,
+            @RequestBody @NotNull @Valid final BoardUpdateRequest request
+    ) {
+        BoardDto response = boardService.updateBoard(boardId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Get board by ID",
+            description = """
+                    This endpoint retrieves the details of a board identified by its ID.
+                    It returns the board's information as a JSON object.
+                    """
+    )
+    @GetMapping("/{boardId}")
+    public ResponseEntity<BoardDto> getBoardById(
+            @PathVariable("boardId") Long boardId
+    ) {
+        BoardDto response = boardService.getBoardById(boardId);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(
+            summary = "Get all boards",
+            description = """
+                    This endpoint retrieves a list of all boards.
+                    It returns the details of each board as a JSON array.
+                    """
+    )
+    @GetMapping("/all")
+    public ResponseEntity<List<BoardDto>> getAllBoards() {
+        List<BoardDto> response = boardService.getAllBoards();
+        return ResponseEntity.ok(response);
+    }
+
 }
---
+++ backend/src/main/java/com/otp/whiteboard/service/BoardService.java
@@ -0,0 +1,198 @@
+package com.otp.whiteboard.service;
+
+import com.otp.whiteboard.dto.board.BoardCreatingRequest;
+import com.otp.whiteboard.dto.board.BoardDto;
+import com.otp.whiteboard.dto.board.BoardUpdateRequest;
+import com.otp.whiteboard.model.Board;
+import com.otp.whiteboard.model.User;
+import com.otp.whiteboard.model.UserBoard;
+import com.otp.whiteboard.repository.BoardRepository;
+import com.otp.whiteboard.repository.UserBoardRepository;
+import com.otp.whiteboard.repository.UserRepository;
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoardService {
+    private  static final Logger logger  = LoggerFactory.getLogger(BoardService.class);
+
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+    private final UserBoardRepository userBoardRepository;
+    public BoardService(BoardRepository boardRepository, UserRepository userRepository, UserBoardRepository userBoardRepository) {
+        this.boardRepository = boardRepository;
+        this.userRepository = userRepository;
+        this.userBoardRepository = userBoardRepository;
+    }
+    /**
+     * Creates a new board based on the provided request.
+     *
+     * @param request the board creation request containing board details.
+     * @return the created board's details as a BoardDto.
+     */
+    @Nonnull
+    public BoardDto createBoard(@Nonnull BoardCreatingRequest request) {
+        logger.debug("Creating Board with name: {}", request.boardName());
+        try{
+            final Optional<Board> exitingBoard = boardRepository.findBoardsByName(request.boardName());
+            if(exitingBoard.isPresent()){
+                logger.warn("Attempt to create board with existing name: {}", request.boardName());
+                throw new IllegalArgumentException("Board already exists with name: " + request.boardName());
+            }
+            Board board = new Board();
+            board.setName(request.boardName());
+            board.setOwnerId(request.ownerId());
+
+            final Board newBoard = boardRepository.save(board);
+            logger.info("Board created successfully with ID: {} and name: {}", newBoard.getId(), newBoard.getName());
+            return new BoardDto(newBoard);
+        }catch (Exception error){
+            logger.error("Error during board creation: {}");
+            throw error;
+        }
+    }
+
+    /**
+     * Retrieves all boards.
+     *
+     * @return a list of all boards as BoardDto objects.
+     */
+    @Nonnull
+    public List<BoardDto> getAllBoards() {
+        try{
+            List<Board> boards = boardRepository.findAll();
+            return boards.stream().map(BoardDto::new).toList();
+        } catch (Exception error){
+            logger.error("Error during fetching all boards: {}", error.getMessage());
+            throw error;
+        }
+    }
+
+    /**
+     * Retrieves a board by its ID.
+     *
+     * @param boardId the ID of the board to be retrieved.
+     * @return the board's details as a BoardDto.
+     * @throws IllegalArgumentException if the board is not found.
+     */
+    @Nonnull
+    public BoardDto getBoardById(@NotNull Long boardId) {
+        try{
+            Optional<Board> optionalBoard = boardRepository.findById(boardId);
+            if (optionalBoard.isEmpty()) {
+                throw new IllegalArgumentException("Board not found with ID: " + boardId);
+            }
+            Board board = optionalBoard.get();
+            return new BoardDto(board);
+        } catch (Exception error){
+            logger.error("Error during fetching board by id: {}", error.getMessage());
+            throw error;
+        }
+    }
+
+    /**
+     * Adds a user to a board.
+     *
+     * @param boardId the ID of the board.
+     * @param userId  the ID of the user to be added.
+     * @return the updated board's details as a BoardDto.
+     * @throws IllegalArgumentException if the board or user is not found, or if the user is already in the board.
+     */
+    @Nonnull
+    public BoardDto addUserToBoard(@NotNull Long boardId, @NotNull Long userId) {
+        try{
+            Board board = boardRepository.findById(boardId)
+                    .orElseThrow(() -> new IllegalArgumentException("Board not found with ID: " + boardId));
+
+            User user = userRepository.getUsersById(userId);
+            if (user == null) throw new IllegalArgumentException("User not found with ID: " + userId);
+
+            UserBoard isUserExitsInBoard = userBoardRepository.findUserBoardByBoardIdAndUserId(boardId, userId);
+            if (isUserExitsInBoard != null) {
+                throw new IllegalArgumentException("User with ID: " + userId + " is already in board with ID: " + boardId);
+            }
+
+            UserBoard userBoard = new UserBoard( user,board);
+            userBoardRepository.save(userBoard);
+
+            board.addUser(user);
+            boardRepository.save(board);
+
+            return new BoardDto(board);
+        } catch (Exception error){
+            logger.error("Error during adding user to board", error);
+            throw error;
+        }
+    }
+
+    /**
+     * Removes a user from a board.
+     *
+     * @param boardId the ID of the board.
+     * @param userId  the ID of the user to be removed.
+     * @return the updated board's details as a BoardDto.
+     * @throws IllegalArgumentException if the board or user is not found, or if the user is not associated with the board.
+     */
+    @Nonnull
+    public BoardDto removeUserFromBoard(@NotNull Long boardId, @NotNull Long userId) {
+        try{
+            User user = userRepository.getUsersById(userId);
+            if (user == null) throw new IllegalArgumentException("User not found with ID: " + userId);
+
+            UserBoard userBoard = userBoardRepository.findUserBoardByBoardIdAndUserId(boardId,userId);
+            if (userBoard == null) {
+                throw new IllegalArgumentException("User with ID: " + userId + " is not associated with any board");
+            }
+
+            Board board = userBoard.getBoard();
+            board.removeUser(user);
+            boardRepository.save(board);
+
+            userBoardRepository.delete(userBoard);
+
+            return new BoardDto(board);
+        } catch (Exception error){
+            logger.error("Error during removing user from board", error);
+            throw error;
+        }
+    }
+
+    /**
+     * Updates an existing board's details.
+     *
+     * @param boardId the ID of the board to be updated.
+     * @param request the board update request containing new details.
+     */
+    @Nonnull
+    public BoardDto updateBoard(@NotNull final Long boardId, @NotNull @Valid final BoardUpdateRequest request) {
+        logger.debug("Updating Board with ID: {}", boardId);
+        try {
+            final Board board = boardRepository.findById(boardId)
+                    .orElseThrow(() -> new IllegalArgumentException("Board not found with ID: " + boardId));
+            updateBoardFields(board, request);
+            boardRepository.save(board);
+            logger.info("Board updated successfully with ID: {}", board.getId());
+            return new BoardDto(board);
+        } catch (Exception error) {
+            logger.error("Error during board update: {}", error.getMessage());
+            throw error;
+        }
+    }
+
+
+    private void updateBoardFields(Board board, BoardUpdateRequest request){
+        if(request.boardName() != null){
+            board.setName(request.boardName());
+        }
+        if(request.numberOfStrokes() != null){
+            board.setNumberOfStrokes(request.numberOfStrokes());
+        }
+    }
+}
